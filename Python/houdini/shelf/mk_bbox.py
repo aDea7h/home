085cc7bb655698d2c2ebf2c5d4be305232368e58
aDea7h <42253593+aDea7h@users.noreply.github.com>
2022-10-27 12:33:37 +0200

    Add files via upload

---
+++ Python/houdini/shelf/mk_bbox.py
@@ -0,0 +1,18 @@
+import hou
+
+def create():
+    node = hou.selectedNodes()
+    if node != ():
+        node = node[0]
+        position = node.position()
+        bbox = node.geometry().boundingBox()
+        print(bbox)
+      
+        size = [bbox.sizevec()[0], bbox.sizevec()[1], bbox.sizevec()[2]]
+        center = [bbox.center()[0], bbox.center()[1], bbox.center()[2]]
+      
+        newNode = node.parent().createNode("box", "Bound_{}".format(node.name()))
+        newNode.setParms({'sizex':size[0], 'sizey':size[1],'sizez':size[2],'tx':center[0],'ty':center[1],'tz':center[2]})# 'center':bbox.center()})
+        newNode.setPosition(position+hou.Vector2([1.5, 0]))
+if __name__ == '__main__':
+    create()
